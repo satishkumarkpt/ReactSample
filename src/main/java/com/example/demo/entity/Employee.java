6e859b8e71ff69753e1ac818ccf7911620d9bc42
satishkumarkpt <satishkumarkpt@gmail.com>
2026-04-25 20:03:05 +0530

    Create Employee entity

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String position;
+}
